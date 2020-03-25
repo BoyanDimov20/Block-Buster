0e46a37fdd5ef3404f3bcf4551fcc6719d7dc2ea
BoyanDimov20 <44165461+BoyanDimov20@users.noreply.github.com>
2020-03-25 20:14:54 +0200

    Configured Layout

---
+++ ASP.NET Core/Web/Movys.Web/Controllers/BlogsController.cs
@@ -0,0 +1,17 @@
+﻿namespace Movys.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BlogsController : BaseController
+    {
+        public IActionResult Listing()
+        {
+            return this.View();
+        }
+    }
+}
