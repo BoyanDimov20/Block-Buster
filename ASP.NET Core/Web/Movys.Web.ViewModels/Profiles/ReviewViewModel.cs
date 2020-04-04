11907f834d669e5b837fdec7694b0d0fd2f3c184
BoyanDimov20 <44165461+BoyanDimov20@users.noreply.github.com>
2020-04-05 00:21:00 +0300

    Profile Controller Modified

---
+++ ASP.NET Core/Web/Movys.Web.ViewModels/Profiles/ReviewViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace Movys.Web.ViewModels.Profiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using Movys.Data.Models;
+    using Movys.Services.Mapping;
+
+    public class ReviewViewModel : IMapFrom<Review>
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Rating { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string MovieId { get; set; }
+
+        public string MovieImageUrl { get; set; }
+
+        public string MovieTitle { get; set; }
+
+        public string MovieReleaseYear { get; set; }
+
+        public string UserId { get; set; }
+
+        public string UserUserName { get; set; }
+    }
+}
--- ASP.NET Core/Web/Movys.Web/Controllers/ProfilesController.cs
+++ ASP.NET Core/Web/Movys.Web/Controllers/ProfilesController.cs
@@ -3,18 +3,35 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Claims;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc;
+    using Movys.Services.Data;
+    using Movys.Web.ViewModels.Profiles;
 
     public class ProfilesController : BaseController
     {
+        private readonly IReviewsService reviewsService;
+
+        public ProfilesController(IReviewsService reviewsService)
+        {
+            this.reviewsService = reviewsService;
+        }
+
         public IActionResult UserProfile()
         {
             return this.View();
         }
 
         public IActionResult UserRating()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            IEnumerable<ReviewViewModel> viewModel = this.reviewsService.GetAll<ReviewViewModel>().Where(x => x.UserId == userId);
+            return this.View(viewModel);
+        }
+
+        public IActionResult UserFavouriteList()
         {
             return this.View();
         }
